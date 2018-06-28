f17e17090c5656406921610af2ce7f1df5c820fe
Tarciso Filho <tarcisofilho@MacBook-Pro-de-Tarciso.local>
2018-06-28 10:45:29 -0300

    Criação Projeto Maven

---
+++ src/main/java/br/ufal/ic/Sistema_de_cadastro_de_notas/App.java
@@ -0,0 +1,13 @@
+package br.ufal.ic.Sistema_de_cadastro_de_notas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
